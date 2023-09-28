f8a35b025d3b72cc55fad77f2f5fa028d0afcc85
Qwertua <wertua17062004@gmail.com>
2023-09-28 21:22:34 +0300

    Инициализация бота,при вводе команды /start бот отображает следующий текст "Hi, " + nameofUser(тег)

--- src/main/java/com/example/telegrambotapplication/TelegramBotApplication.java
+++ src/main/java/com/example/telegrambotapplication/TelegramBotApplication.java
@@ -2,12 +2,14 @@ package com.example.telegrambotapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.generics.LongPollingBot;
 
 @SpringBootApplication
 public class TelegramBotApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TelegramBotApplication.class, args);
+
     }
 
 }
